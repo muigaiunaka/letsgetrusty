24767785a9efc07e6a92c9bfa5e237f95980f2bc
muigaiunaka <muigaiunaka@gmail.com>
2018-10-18 15:19:29 -0400

    rust file that prints hello world

---
+++ 00-hello-world/00-hello-world.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // prints the line hello world
+    println!("Hello, World!");
+    // then A binary can be generated using the Rust compiler: rustc.
+    // `rustc 00-hello-world`
+    // this should create a file that matches the name of the file
+}
